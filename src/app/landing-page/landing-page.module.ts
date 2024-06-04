036de919302176ee77b76a8031b32d8779ee6689
mohamed.bouhari <mohamed.bouhari@indyli-services.com>
2024-06-04 21:20:57 +0100

    création du log In et de la page d'inscription.

--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -2,19 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { SharedModule } from '../shared/shared.module';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 
 @NgModule({
   declarations: [
-    LandingPageComponent
+    LandingPageComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
     SharedModule
   ],
   exports: [
-    LandingPageComponent
+    LandingPageComponent,
+    RegisterComponent,
+    LoginComponent
   ]
 })
 export class LandingPageModule { }
